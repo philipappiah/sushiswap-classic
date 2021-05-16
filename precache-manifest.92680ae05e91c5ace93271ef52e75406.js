5fb291a55fa795df64f955ed0cb2f9a298fe9bf2
unknown <pappiahkubi1@gmail.com>
2021-05-16 17:07:10 +0000

    Updates

---
+++ precache-manifest.92680ae05e91c5ace93271ef52e75406.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4b2aebde70589691e763e021e2eeb9",
+    "url": "/sushiswap-classic/index.html"
+  },
+  {
+    "revision": "881fe259b6ff1b6d3117",
+    "url": "/sushiswap-classic/static/css/main.e26f5612.chunk.css"
+  },
+  {
+    "revision": "9dd86aa4faf085addd96",
+    "url": "/sushiswap-classic/static/js/0.5ccd20d9.chunk.js"
+  },
+  {
+    "revision": "41d99751b9bc17160c2d",
+    "url": "/sushiswap-classic/static/js/10.6dc86fdf.chunk.js"
+  },
+  {
+    "revision": "e27ad67c5b3cbfe792fe",
+    "url": "/sushiswap-classic/static/js/11.9712b041.chunk.js"
+  },
+  {
+    "revision": "0862d584ad4c6c742f18",
+    "url": "/sushiswap-classic/static/js/3.7dd9da0f.chunk.js"
+  },
+  {
+    "revision": "433afdb71c519c5428db23e5131a50a9",
+    "url": "/sushiswap-classic/static/js/3.7dd9da0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547815e6ac27453ab66c",
+    "url": "/sushiswap-classic/static/js/4.ce3990b6.chunk.js"
+  },
+  {
+    "revision": "24b02bf28789bab9e729",
+    "url": "/sushiswap-classic/static/js/5.cab0e4f9.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/sushiswap-classic/static/js/5.cab0e4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fd9ba6a3ba9a2fdf67",
+    "url": "/sushiswap-classic/static/js/6.c711903a.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/sushiswap-classic/static/js/6.c711903a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940c94240854584a2300",
+    "url": "/sushiswap-classic/static/js/7.5ceaa28e.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/sushiswap-classic/static/js/7.5ceaa28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f8879d281f6d702b3d5",
+    "url": "/sushiswap-classic/static/js/8.8d81b04e.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/sushiswap-classic/static/js/8.8d81b04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375a15b15489576ae512",
+    "url": "/sushiswap-classic/static/js/9.b17a809c.chunk.js"
+  },
+  {
+    "revision": "881fe259b6ff1b6d3117",
+    "url": "/sushiswap-classic/static/js/main.2680d69b.chunk.js"
+  },
+  {
+    "revision": "effec1cd74f68d1c686b",
+    "url": "/sushiswap-classic/static/js/runtime-main.7d817403.js"
+  },
+  {
+    "revision": "ec78e8e86a08b2278fa9baf09388d697",
+    "url": "/sushiswap-classic/static/media/chef.ec78e8e8.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/sushiswap-classic/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "/sushiswap-classic/static/media/wallet-connect.d1f73612.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5ccdbc2036ca23aa38c59930c26da5d7.js"
+  "/sushiswap-classic/precache-manifest.92680ae05e91c5ace93271ef52e75406.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sushiswap-classic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
